dd54029bd313e85c0a1cca55db0a6229234973aa
NateTewolde <tewolde.nate@gmail.com>
2022-05-20 12:52:28 -0700

    Comments on each funcation with a brief description and a working 5 round player vs computer rock paper scissors game

--- myscripts.js
+++ myscripts.js
@@ -1,62 +1,79 @@
+game();
 
-
-const playerSelection = 'Rock';
-game ();
-
-
-function game (){
-    let gameResultPlayer = 0;
-    let gameResultComputer = 0;
-    for (let i = 1; i <= 5; i++){
-        const computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if (roundResult.includes("win")){
-            gameResultPlayer++;
-        }else if (roundResult.includes('lose')){
-            gameResultComputer++;
-        } 
+//Runs 5 rounds of playRound and displays the results
+function game() {
+  let gameResultPlayer = 0;
+  let gameResultComputer = 0;
+  for (let i = 1; i <= 5; i++) {
+    const playerSelection = capitalize(prompt("Rock Paper Scissors GO!"));
+    const computerSelection = computerPlay();
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+    if (roundResult.includes("win")) {
+      gameResultPlayer++;
+    } else if (roundResult.includes("lose")) {
+      gameResultComputer++;
     }
-    console.log(`player: ${gameResultPlayer}\ncomputer: ${gameResultComputer}`);
-}
-
-function playRound (playerSelection, computerSelection) {
-    console.log(`computerSelection:${computerSelection}\n playerSelection: ${playerSelection}`);
-    if ( (playerSelection === 'Rock' && computerSelection === 'Paper')
-    || (playerSelection === 'Paper' && computerSelection === 'Scissors')
-    || (playerSelection === 'Scissors' && computerSelection === 'Rock')){
-        return(`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (playerSelection === computerSelection){
-        return (`Draw! ${playerSelection} is equal to ${computerSelection}`);
-    } else {
-        return(`You win! ${playerSelection} beats ${computerSelection}`);
-    }
-
-}
-
-function computerPlay () {
-    let randomNum = getRandomIntInclusive(0,2);
-    let computerChoice;
-    switch (randomNum){
-        case 0:
-            computerChoice = 'Rock';
-            break;
-        case 1:
-            computerChoice = 'Paper';
-            break;
-        case 2:
-            computerChoice = 'Scissors';
-            break;
-        default: 
-            console.log("Something went wrong with randomNum");
-    }
-    return computerChoice;
-}
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
+  console.log(`player: ${gameResultPlayer}\ncomputer: ${gameResultComputer}`);
+  if (gameResultPlayer > gameResultComputer) {
+    console.log(
+      `You won the game! ${gameResultPlayer} to ${gameResultComputer}`
+    );
+  } else if (gameResultComputer > gameResultPlayer) {
+    console.log(
+      `You lose the game! ${gameResultComputer} to ${gameResultPlayer}`
+    );
+  }
+}
 
+//Takes 2 variables and plays a round of rock paper scissors with them
+function playRound(playerSelection, computerSelection) {
+  console.log(
+    `computerSelection:${computerSelection}\n playerSelection: ${playerSelection}`
+  );
+  if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `Draw! ${playerSelection} is equal to ${computerSelection}`;
+  } else {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  }
+}
 
+//Returns random computer choice of rock paper scissors
+function computerPlay() {
+  let randomNum = getRandomIntInclusive(0, 2);
+  let computerChoice;
+  switch (randomNum) {
+    case 0:
+      computerChoice = "Rock";
+      break;
+    case 1:
+      computerChoice = "Paper";
+      break;
+    case 2:
+      computerChoice = "Scissors";
+      break;
+    default:
+      console.log("Something went wrong with randomNum");
+  }
+  return computerChoice;
+}
+
+//Helper function for computerPlay, returns a random number between and including a mix/max
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+//Returns a string with only the first letter capitalized.
+function capitalize(sent) {
+  let rightSent = sent.charAt(0).toUpperCase() + sent.slice(1).toLowerCase();
+  return rightSent;
+}
